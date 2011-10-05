70c7f46db692c342c8d0d5ffb7901a5018e15de3
Artur Egorov <artur@egorov.in>
2011-10-05 14:03:49 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pumpkin::Application.config.secret_token = '38bd3920f4c0e8ba4d8a9be9bbdd4f00ed3b7add4d7efb18c1b0730880aec0f44b65e455761e53a12dc660e5c0176df13270bad5c457a0fa44d1881cbef97327'
