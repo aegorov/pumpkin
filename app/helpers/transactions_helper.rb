13eb716f51c6d139717b638439bff76c040ef198
Artur Egorov <artur@egorov.in>
2011-11-11 04:39:42 +0300

    show_status helper method

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,11 @@
+# encoding: UTF-8
 module TransactionsHelper
+	def show_status(status)
+		case status
+			when "send" 
+				"<span class='label'>Отправлено</span>".html_safe
+			when "received"
+				"<span class='label success'>Получено</span>".html_safe
+		end
+	end
 end
